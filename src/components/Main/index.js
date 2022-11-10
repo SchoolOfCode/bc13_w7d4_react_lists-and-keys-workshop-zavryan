fe7c4c2acb9876073c4fafb7f4f5af8663bdffc3
mvhmz81 <mvhmz@yahoo.co.uk>
2022-11-10 20:26:14 +0000

    stuck on last task

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,18 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import './index.css';
+import "./index.css";
 
-import samplePosts from '../../libs/data';
+import samplePosts from "../../libs/data";
 
-import Post from '../Post';
+import Post from "../Post";
 
 function Main() {
   const [posts, setPosts] = useState(samplePosts);
-
-  return <main id="main"> {
-    posts.map((posty) =>{
-      return <Post key = {posty.postId} {...posty}/> 
-  })}</main>;
+  return (
+    <main id="main">
+      {posts.map(({ postId, title, date, author, text, highlights, img }) => {
+        return (
+          <Post
+            key={postId}
+            title={title}
+            date={date}
+            author={author}
+            text={text}
+            highlights={highlights}
+            img={img}
+          />
+        );
+      })}
+    </main>
+  );
 }
 
 export default Main;
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import './index.css';
+import "./index.css";
 
-function Post() {
-  return <article></article>;
+function Post({ title, date, author, text, highlights, img }) {
+  return (
+    <article>
+      <h1>{title}</h1>
+      <time>{date}</time>
+      <h3>{author}</h3>
+      <p>{text}</p>
+      {highlights.map((prop) => {
+        return <mark key={prop}>{prop}</mark>;
+      })}
+      <img src={img.link} alt={img.alt}></img>
+    </article>
+  );
 }
 
 export default Post;
